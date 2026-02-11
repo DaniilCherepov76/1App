b92ca0b3777b7116af4af68710027c2587d54abb
Daniil <pz38na79@gmail.com>
2026-02-11 21:25:16 +0300

    init empty window

---
+++ 1App.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+
+window = Tk()
+window.title ("Mini app")
+window.geometry("720x360")
+window.resizable(True, True)
+
+
+window.mainloop()
