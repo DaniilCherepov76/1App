a4b90e021aa04fc26608d1f9a4e9daad2958ec89
Daniil <pz38na79@gmail.com>
2026-02-12 00:01:27 +0300

    Add 5-pos history box

--- 1App.py
+++ 1App.py
@@ -2,22 +2,39 @@ from tkinter import *
 from tkinter import messagebox
 import random
 
-
-def rand_int():
-    messagebox.showinfo(None,random.randint(1,10) )
-
-
 window = Tk()
 window.title ("Mini app")
 window.geometry("720x360")
 window.resizable(True, True)
 
+last_num = []
+                                #defs#
+def size5_chek():
+    if len(last_num)==5 :
+        last_num.clear()
+
+def rand_int():
+    num = random.randint(1,10)
+    messagebox.showinfo(None, num)
+    last_num.insert(0, num)
+    history.config(text=' '.join(map(str, last_num)))
+    size5_chek()
+                              #Elements#
 lbl = Label(window,
             text='Добавить число',
             font=('Arial bold', 16),
             fg='black',                  # Цвет текста
             bg='white')                # Цвет фона
 lbl.place(x=290, y=25)
+
+history = Label(window,
+            text=' ',
+            font=('Arial bold', 22),
+            fg='black',
+            width=12,
+            height=1,                  # Цвет текста
+            bg='green')                # Цвет фона
+history.place(x=290, y=290)
 
 
 # enter_text = Entry(window,
